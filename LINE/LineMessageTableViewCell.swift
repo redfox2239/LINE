d515dc6b70950828a08245b985458e6d0b499a86
HARADA REO <rharada@golyat.local>
2016-05-22 03:33:49 +0900

    first commit

---
+++ LINE/LineMessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LineMessageTableViewCell.swift
+//  LINE
+//
+//  Created by HARADA REO on 2015/09/14.
+//  Copyright (c) 2015年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class LineMessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
